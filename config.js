0f3a0aa523cc8e9d72749e21caf915a3207b31b9
Dnyanesh Patil <dpatil@netgear.com>
2016-11-16 00:16:24 -0800

    modified and adding cofig file

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ exports.config = {
   // to the location of this config. If no other method of starting selenium
   // is found, this will default to
   // node_modules/protractor/selenium/selenium-server...
-  seleniumServerJar: './selenium-server-standalone-2.53.0.jar',
+  seleniumServerJar: './example/selenium-server-standalone-2.53.0.jar',
   specs: ['example/*/test/functional/calculationTest.spec.js'],
 
   // The port to start the selenium server on, or null if the server should
